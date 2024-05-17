001e49a6aaa23f56ac09a7cc87f22cfc2e5c1708
Aarthi <aarthielumalaimaha@gmail.com>
2024-05-17 18:07:23 +0530

    socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
   const { authUser } = useAuthContext();
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://mern-chat-app-cpcr.onrender.com", {
         query: {
           userId: authUser._id,
         },
